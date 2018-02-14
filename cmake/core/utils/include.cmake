f49aa8692bb5a4f79f0f08e0da4b48e3520ff776
叶小琴 <weixiao@kokotoAir.local>
2018-02-14 01:02:42 +0100

    Draft cmake of skia for mac osx.

--- cmake/core/utils/include.cmake
+++ cmake/core/utils/include.cmake
@@ -3,8 +3,6 @@
 if(MSVC)
 add_definitions(-DHAVE_MS_C_RUNTIME)
 add_definitions(-DHAVE_WIN32_THREADS)
-#add_definitions(-D__BEGIN_DECLS= )
-#add_definitions(-D__END_DECLS= )
 add_definitions(-D__PRETTY_FUNCTION__=__FUNCTION__)
 include_directories(AFTER ${M3E_SOURCE_DIR}/src/core/msvc)
 
--- cmake/external/skia/include.cmake
+++ cmake/external/skia/include.cmake
@@ -1,3 +1,8 @@
 
+if(APPLE)
+#add_definitions(-D_MACOSX)
+set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-std=c++11 -stdlib=libc++")
+endif()
+
 # include
 include_directories(AFTER ${M3E_SOURCE_DIR}/${EXT_PATH}/skia/include)
