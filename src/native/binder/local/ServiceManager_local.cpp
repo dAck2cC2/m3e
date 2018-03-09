25fd3094d3a57f104a0f79bafc47df82cc061c89
叶小琴 <weixiao@kokotoAir.local>
2018-03-10 00:24:03 +0100

    implement the ServiceManager

--- src/native/binder/local/ServiceManager_local.cpp
+++ src/native/binder/local/ServiceManager_local.cpp
@@ -1,49 +1,78 @@
 #define LOG_TAG "ServiceManager"
 
-#include <utils/threads.h>
-
 #include <binder/IServiceManager.h>
 #include <binder/ProcessState.h>
 #include <binder/IPCThreadState.h>
+#include <binder/Status.h>
 
-//#include <private/binder/binder_module.h>
+#include <utils/threads.h>
+#include <utils/KeyedVector.h>
 
 namespace android {
 
-class LocalServiceManager : public BnInterface<IServiceManager>, public Thread
+class LocalServiceManager : public BBinder, public Thread
 {
 public:
-	LocalServiceManager() {  };
-
-    /**
-     * Retrieve an existing service, blocking for a few seconds
-     * if it doesn't yet exist.
-     */
-    virtual sp<IBinder>         getService( const String16& name) const {return NULL;};
-
-    /**
-     * Retrieve an existing service, non-blocking.
-     */
-    virtual sp<IBinder>         checkService( const String16& name) const {return NULL;};
-
-    /**
-     * Register a service.
-     */
-    virtual status_t            addService( const String16& name,
-                                            const sp<IBinder>& service,
-                                            bool allowIsolated = false) {return OK;};
-
-    /**
-     * Return list of all existing services.
-     */
-    virtual Vector<String16>    listServices() {return mServices;};
-
+    LocalServiceManager() : mDescriptor("android.os.IServiceManager") {  };
+    virtual const String16& getInterfaceDescriptor() const { return mDescriptor; };
+    
     virtual status_t onTransact( 
             uint32_t code,
             const Parcel& data,
             Parcel* reply,
             uint32_t flags = 0)
     {
+        switch (code) {
+            case IServiceManager::CHECK_SERVICE_TRANSACTION:
+            {
+                CHECK_INTERFACE(IServiceManager, data, reply);
+                String16 name = data.readString16();
+                
+                sp<IBinder> binder;
+                ssize_t n = mServices.indexOfKey(name);
+                if (n >= 0) {
+                    binder = mServices.valueFor(name);
+                }
+                
+                reply->writeStrongBinder(binder);
+            }
+                break;
+            case IServiceManager::ADD_SERVICE_TRANSACTION:
+            {
+                CHECK_INTERFACE(IServiceManager, data, reply);
+                String16 name = data.readString16();
+                sp<IBinder> binder = data.readStrongBinder();
+                int32_t allowIsolated = data.readInt32();
+                ssize_t n = mServices.indexOfKey(name);
+                if (n < 0) {
+                    n = mServices.add(name, binder);
+                }
+                if (n < 0) {
+                    reply->writeInt32(n);
+                } else {
+                    reply->writeInt32(binder::Status::EX_NONE);
+                }
+            }
+                break;
+            case IServiceManager::LIST_SERVICES_TRANSACTION:
+            {
+                CHECK_INTERFACE(IServiceManager, data, reply);
+                int32_t index = data.readInt32();
+                
+                status_t ret = BAD_INDEX;
+                String16 name;
+                if (index < mServices.size()) {
+                    name = mServices.keyAt(index);
+                    ret = NO_ERROR;
+                }
+                reply->writeString16(name);
+                return (ret);
+            }
+            case IServiceManager::GET_SERVICE_TRANSACTION:
+            default:
+                return BBinder::onTransact(code, data, reply, flags);
+        }
+        
         return OK;
     };
 
@@ -56,9 +85,9 @@ private:
 		sp<ProcessState>  proc = ProcessState::self();
         proc->becomeContextManager(NULL, NULL);
 
-        sp<IBinder> thiz = IInterface::asBinder(this);
+        //sp<IBinder> thiz = IInterface::asBinder(this);
         Parcel parcel;
-        parcel.writeStrongBinder(thiz);
+        parcel.writeStrongBinder(this);
         IPCThreadState::self()->transact(0, IBinder::PING_TRANSACTION, parcel, NULL, IBinder::FLAG_ONEWAY);
 
 		IPCThreadState::self()->joinThreadPool();
@@ -67,9 +96,11 @@ private:
     };
     
 private:
-    Vector<String16>  mServices;
+    const android::String16              mDescriptor;
+    KeyedVector<String16, sp<IBinder> >  mServices;
 };
 
+    
 class LocalServiceManagerInit
 {
 public:
@@ -82,6 +113,7 @@ private:
 	sp<LocalServiceManager> sm;
 };
 
+    
 static LocalServiceManagerInit gLocalServiceManager;
 
 }; // namespace android
--- src/native/binder/local/binder_local.cpp
+++ src/native/binder/local/binder_local.cpp
@@ -98,174 +98,224 @@ public:
 	};
 };
 
-#define BINDER_CMD_SIZE  (sizeof(int32_t))
-#define BINDER_TR_SIZE   (sizeof(binder_transaction_data))
-
-status_t binder_write_read_local(int handler, void* data)
+#define BINDER_CMD_SIZE    (sizeof(int32_t))
+#define BINDER_TR_SIZE     (sizeof(binder_transaction_data))
+#define BINDER_TIMEOUT_MS  (500)
+    
+static status_t binder_write_local(int handler, binder_write_read* pbwr, int& bNeedRead, int readTimeout)
 {
-	status_t result = BAD_VALUE;
-	int bNeedRead = 0;
+    status_t result = NO_ERROR;
+    
+    bNeedRead   = 0;
+    readTimeout = -1;
+    
+    if ((pbwr->write_buffer != 0) && (pbwr->write_size < BINDER_CMD_SIZE)) {
+        bNeedRead++;;
+        return NO_ERROR;
+    }
+    if  ((pbwr->write_buffer == 0) && (pbwr->write_size > 0)) {
+        return BAD_VALUE;
+    }
 
-	binder_write_read* pbwr = (binder_write_read*)data;
-	if (!pbwr) return result;
-
-	// write
-	if (pbwr->write_buffer && pbwr->write_size >= BINDER_CMD_SIZE) {
-		int32_t* cmd = (int32_t *)(pbwr->write_buffer);
+    while (pbwr->write_consumed < pbwr->write_size) {
+        uintptr_t data = (uintptr_t)(pbwr->write_buffer + pbwr->write_consumed);
+        size_t    size = pbwr->write_size - pbwr->write_consumed;
+        if (size < BINDER_CMD_SIZE) {
+            bNeedRead = 0;
+            return NOT_ENOUGH_DATA;
+        }
+        
+        int32_t cmd = *((int32_t *)data);
         pbwr->write_consumed += sizeof(int32_t);
-		switch (*cmd) {
-        case BC_REPLY:
+        
+        switch (cmd) {
+            case BC_REPLY:
             {
+                if (size < (BINDER_CMD_SIZE + BINDER_TR_SIZE)) return NOT_ENOUGH_DATA;
+
                 binder_entry* e = binder_lookupHandle_local(handler);
                 if (e == NULL || e->revicerLoop == NULL) return (NO_INIT);
-
-                if (pbwr->write_size < (BINDER_CMD_SIZE + BINDER_TR_SIZE)) return result;
                 
-                binder_transaction_data* tr = (binder_transaction_data *)((uint8_t *)(pbwr->write_buffer) + sizeof(uint32_t));
+                binder_transaction_data* tr = (binder_transaction_data *)(data + BINDER_CMD_SIZE);
                 tr->target.handle = e->senderHandler;
-
+                
             }
             // continue to send this out
-		case BC_TRANSACTION:
-			{
-				if (pbwr->write_size < (BINDER_CMD_SIZE + BINDER_TR_SIZE)) return result;
-
-				binder_transaction_data* tr = (binder_transaction_data *)((uint8_t *)(pbwr->write_buffer) + sizeof(uint32_t));
+            case BC_TRANSACTION:
+            {
+                if (size < (BINDER_CMD_SIZE + BINDER_TR_SIZE)) return NOT_ENOUGH_DATA;
+                
+                binder_transaction_data* tr = (binder_transaction_data *)(data + BINDER_CMD_SIZE);
                 pbwr->write_consumed += sizeof(binder_transaction_data);
-
-				binder_entry* et = binder_lookupHandle_local(tr->target.handle);
-				if (et == NULL || et->revicerLoop == NULL) return (DEAD_OBJECT);
+                
+                binder_entry* et = binder_lookupHandle_local(tr->target.handle);
+                if (et == NULL || et->revicerLoop == NULL) return (DEAD_OBJECT);
                 
                 if (tr->data.ptr.offsets && tr->offsets_size) {
                     result = binder_registerService_local(handler, *tr);
                     if (result) return result;
                 }
-
-                if (BC_TRANSACTION == (*cmd)) {
-                    (*cmd) = BR_TRANSACTION;
-                } else if (BC_REPLY == (*cmd)) {
-                    (*cmd) = BR_REPLY;
-                }
-				tr->target.ptr = 0;
-
-				Parcel* inData = new Parcel();
-				if (!inData) return NO_MEMORY;
-
-				inData->setData((uint8_t *)(pbwr->write_buffer), pbwr->write_size);
-
-				Message evt(handler);
-				sp<MessageHandler> trdata = new BinderMessageHandler(inData, et->cache, et->senderHandler);
-				et->revicerLoop->sendMessage(trdata, evt);
+                //tr->target.ptr = 0;
                 
-                if (pbwr->read_buffer && pbwr->read_size) {
-                    *((int32_t *)(pbwr->read_buffer)) = BR_TRANSACTION_COMPLETE;
-                }
-				bNeedRead = 0;
-			}
-			break;
-		case BC_ENTER_LOOPER:
-		case BC_REGISTER_LOOPER:
-			bNeedRead = 1;
-			break;
-		case BC_EXIT_LOOPER:
-			bNeedRead = 0;
-			break;
-        case BC_ACQUIRE_RESULT:
+                Parcel* inData = new Parcel();
+                if (!inData) return NO_MEMORY;
+                
+                inData->setData((uint8_t *)data, BINDER_CMD_SIZE + BINDER_TR_SIZE);
+                
+                Message evt(handler);
+                sp<MessageHandler> trdata = new BinderMessageHandler(inData, et->cache, et->senderHandler);
+                et->revicerLoop->sendMessage(trdata, evt);
+                
+                bNeedRead++;
+                readTimeout = BINDER_TIMEOUT_MS;
+            }
+                break;
+            case BC_ENTER_LOOPER:
+            case BC_REGISTER_LOOPER:
+                bNeedRead++;
+                break;
+            case BC_EXIT_LOOPER:
+                bNeedRead = 0;
+                return;
+            case BC_ACQUIRE_RESULT:
             {
-                int32_t value = *((int32_t *)(pbwr->write_buffer + pbwr->write_consumed));
+                int32_t value = *((int32_t *)(data + BINDER_CMD_SIZE));
                 pbwr->write_consumed += sizeof(int32_t);
             }
-            break;
-        case BC_FREE_BUFFER:
+                break;
+            case BC_FREE_BUFFER:
             {
-                uintptr_t value = *((uintptr_t *)(pbwr->write_buffer + pbwr->write_consumed));
+                uintptr_t value = *((uintptr_t *)(data + BINDER_CMD_SIZE));
                 pbwr->write_consumed += sizeof(uintptr_t);
             }
-            break;
-        case BC_INCREFS:
+                break;
+            case BC_INCREFS:
             {
-                int32_t value = *((int32_t *)(pbwr->write_buffer + pbwr->write_consumed));
+                int32_t value = *((int32_t *)(data + BINDER_CMD_SIZE));
                 pbwr->write_consumed += sizeof(int32_t);
             }
-            break;
-        case BC_ACQUIRE:
+                break;
+            case BC_ACQUIRE:
             {
-                int32_t value = *((int32_t *)(pbwr->write_buffer + pbwr->write_consumed));
+                int32_t value = *((int32_t *)(data + BINDER_CMD_SIZE));
                 pbwr->write_consumed += sizeof(int32_t);
             }
-            break;
-        case BC_RELEASE:
+                break;
+            case BC_RELEASE:
             {
-                int32_t value = *((int32_t *)(pbwr->write_buffer + pbwr->write_consumed));
+                int32_t value = *((int32_t *)(data + BINDER_CMD_SIZE));
                 pbwr->write_consumed += sizeof(int32_t);
             }
-            break;
-        case BC_DECREFS:
+                break;
+            case BC_DECREFS:
             {
-                int32_t value = *((int32_t *)(pbwr->write_buffer + pbwr->write_consumed));
+                int32_t value = *((int32_t *)(data + BINDER_CMD_SIZE));
                 pbwr->write_consumed += sizeof(int32_t);
             }
-            break;
-        case BC_INCREFS_DONE:
+                break;
+            case BC_INCREFS_DONE:
             {
                 uintptr_t value = *((uintptr_t *)(pbwr->write_buffer + pbwr->write_consumed));
                 pbwr->write_consumed += sizeof(uintptr_t);
                 value = *((uintptr_t *)(pbwr->write_buffer + pbwr->write_consumed));
                 pbwr->write_consumed += sizeof(uintptr_t);
             }
-            break;
-        case BC_ACQUIRE_DONE:
+                break;
+            case BC_ACQUIRE_DONE:
             {
                 uintptr_t value = *((uintptr_t *)(pbwr->write_buffer + pbwr->write_consumed));
                 pbwr->write_consumed += sizeof(uintptr_t);
                 value = *((uintptr_t *)(pbwr->write_buffer + pbwr->write_consumed));
                 pbwr->write_consumed += sizeof(uintptr_t);
             }
-            break;
-        case BC_ATTEMPT_ACQUIRE:
+                break;
+            case BC_ATTEMPT_ACQUIRE:
             {
                 int32_t value = *((int32_t *)(pbwr->write_buffer + pbwr->write_consumed));
                 pbwr->write_consumed += sizeof(int32_t);
                 value = *((int32_t *)(pbwr->write_buffer + pbwr->write_consumed));
                 pbwr->write_consumed += sizeof(int32_t);
             }
-            break;
-        case BC_REQUEST_DEATH_NOTIFICATION:
-        case BC_CLEAR_DEATH_NOTIFICATION:
-        case BC_DEAD_BINDER_DONE:
-            break;
-		default:
-            assert(0);
-			break;
-		}
-    } else {
-        bNeedRead = 1;
+                break;
+            case BC_REQUEST_DEATH_NOTIFICATION:
+            case BC_CLEAR_DEATH_NOTIFICATION:
+            case BC_DEAD_BINDER_DONE:
+                break;
+            default:
+                assert(0);
+                break;
+        } // switch
+    } // while
+
+    return result;
+}
+
+status_t binder_read_local(int handler, binder_write_read* pbwr, int readTimeout)
+{
+    status_t result = NO_ERROR;
+    
+    if ((pbwr->read_buffer == 0) || (pbwr->read_size == 0)) {
+        return NO_ERROR;
+    }
+    int32_t* cmd = (int32_t *)(pbwr->read_buffer);
+
+    binder_entry* e = binder_lookupHandle_local(handler);
+    if (e == NULL || e->revicerLoop == NULL) return (NO_INIT);
+    
+    int ret = e->revicerLoop->pollOnce(readTimeout);
+    if (Looper::POLL_TIMEOUT == ret) {
+        (*cmd) = BR_DEAD_REPLY;
+        return NO_ERROR;
+    }
+    
+    if (e->cache != NULL && e->cache->dataSize() <= pbwr->read_size) {
+        memcpy((void *)(pbwr->read_buffer), e->cache->data(), e->cache->dataSize());
+        pbwr->read_consumed = e->cache->dataSize();
+        binder_transaction_data* tr = (binder_transaction_data *)((uint8_t *)(pbwr->read_buffer) + sizeof(uint32_t));
+        tr->target.ptr = (void *)(e->service.binder);
+        tr->cookie = (void *)(e->service.cookie);
+        
+        if (BC_TRANSACTION == (*cmd)) {
+            (*cmd) = BR_TRANSACTION;
+            
+            binder_entry* eSender = binder_lookupHandle_local(e->senderHandler);
+            
+            Parcel* inData = new Parcel();
+            if (!inData) return NO_MEMORY;
+            inData->writeInt32(BR_TRANSACTION_COMPLETE);
+            
+            Message evt(handler);
+            sp<MessageHandler> trdata = new BinderMessageHandler(inData, eSender->cache, eSender->senderHandler);
+            eSender->revicerLoop->sendMessage(trdata, evt);
+            
+        } else if (BC_REPLY == (*cmd)) {
+            (*cmd) = BR_REPLY;
+        }
+    }
+    
+    if (e->cache) {
+        delete (e->cache);
+        e->cache = NULL;
     }
 
+    return result;
+}
+
+status_t binder_write_read_local(int handler, void* data)
+{
+	status_t result = BAD_VALUE;
+	int bNeedRead = 0;
+    int readTimeout = -1;
+
+	binder_write_read* pbwr = (binder_write_read*)data;
+	if (!pbwr) return result;
     
-	// read
-	result = NO_ERROR;
-	if (bNeedRead && pbwr->read_buffer && pbwr->read_size) {
-        binder_entry* e = binder_lookupHandle_local(handler);
-        if (e == NULL || e->revicerLoop == NULL) return (NO_INIT);
-
-		int ret = e->revicerLoop->pollOnce(-1);
-		//if (Looper::POLL_CALLBACK != ret) return NO_ERROR;
-
-		if (e->cache != NULL && e->cache->dataSize() <= pbwr->read_size) {
-			memcpy((void *)(pbwr->read_buffer), e->cache->data(), e->cache->dataSize());
-            pbwr->read_consumed = e->cache->dataSize();
-            binder_transaction_data* tr = (binder_transaction_data *)((uint8_t *)(pbwr->read_buffer) + sizeof(uint32_t));
-            tr->target.ptr = (void *)(e->service.binder);
-            tr->cookie = (void *)(e->service.cookie);
-		}
-
-		if (e->cache) {
-			delete (e->cache);
-			e->cache = NULL;
-		}
-	}
-
+    result = binder_write_local(handler, pbwr, bNeedRead, readTimeout);
+    
+    if (bNeedRead) {
+        result = binder_read_local(handler, pbwr, readTimeout);
+    }
+    
 	return result;
 }
 
