c02a1ccb4bc15572727ef55e2fd071d892e8a2d4
叶小琴 <weixiao@kokotoAir.local>
2018-05-12 01:26:40 +0200

    Add libraries of wxWidgets.

--- cmake/external/wxwidgets/include.cmake
+++ cmake/external/wxwidgets/include.cmake
@@ -1,2 +1,7 @@
 
+if (APPLE)
+    add_definitions(-D__WXOSX_COCOA__=1)
+endif()
+
+include_directories(AFTER ${M3E_BINARY_DIR}/wxWidgets)
 include_directories(AFTER ${M3E_SOURCE_DIR}/${EXT_PATH}/wxWidgets/include)
