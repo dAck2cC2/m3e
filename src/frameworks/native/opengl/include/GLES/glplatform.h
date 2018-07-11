615bf863f3e79697b0f9fb8a3db62ed206ee39fe
feng <feng@fengs-Mac.local>
2018-07-11 17:57:14 +0200

    Fix building error of Mac OSX

--- src/frameworks/native/opengl/include/GLES/glplatform.h
+++ src/frameworks/native/opengl/include/GLES/glplatform.h
@@ -6,6 +6,10 @@
 #if defined(_WIN32)
 #	include <GL/GL.h>
 #	include <GL/GLU.h>
+#elif defined(__APPLE__)
+#   include <OpenGL/gl.h>
+#else
+#   error "Unknow platform for OpenGL"
 #endif 
 
 #endif /* __glplatform_h_ */
