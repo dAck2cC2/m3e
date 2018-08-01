4d385ca21dad54c70707091749b57361cffded6e
dAck2cC2 <sable_d@163.com>
2018-08-01 12:49:14 +0200

    Use makefile of EGL for both native one and angle.

--- cmake/frameworks/native/opengl/EGL/include.cmake
+++ cmake/frameworks/native/opengl/EGL/include.cmake
@@ -1,9 +1,16 @@
 
-find_package(OpenGL REQUIRED)
-set(OPENGL_DEPLIB 
-EGL 
-${OPENGL_gl_LIBRARY}
-)
+# EGL and OpenGL
+if (ENABLE_NATIVE_GL)
+    add_definitions(-DENABLE_NATIVE_GL=1)
 
-# include
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/frameworks/native/opengl/include)
+    find_package(OpenGL REQUIRED)
+    set(OPENGL_DEPLIB EGL ${OPENGL_gl_LIBRARY})
+
+    include_directories(AFTER ${M3E_SOURCE_DIR}/src/frameworks/native/opengl/include)
+
+elseif (ENABLE_ANGLE)
+    set(OPENGL_DEPLIB  angle)
+    
+    include(${M3E_SOURCE_DIR}/cmake/external/angle/include.cmake)
+
+endif()
