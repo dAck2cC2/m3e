ab055012a003fcf25488fc98705d19d00fccc2bc
dAck2cC2 <sable_d@163.com>
2019-01-17 17:56:55 +0100

    Move system header files to right place as Android 8.1

--- cmake/hardware/libhardware/include.cmake
+++ cmake/hardware/libhardware/include.cmake
@@ -1,6 +1,8 @@
 # define
 DLL_PUBLIC(HARDWARE)
 
+include(${M3E_SOURCE_DIR}/cmake/core/system/include.cmake)
+
 # include
 include_directories(AFTER ${M3E_SOURCE_DIR}/src/hardware/libhardware/include)
 include_directories(AFTER ${M3E_SOURCE_DIR}/src/system/media/audio/include)
