37851d7330b1110dc1ec41ac3d3749fa275f53e5
Jeremy Feng <dAck2cC@yahoo.com>
2020-06-16 20:53:49 +0200

    Fix building error of MSVC

--- cmake/external/angle/include.cmake
+++ cmake/external/angle/include.cmake
@@ -1,6 +1,8 @@
 
 # include
-add_definitions(-DEGL_EGLEXT_PROTOTYPES)
-add_definitions(-DGL_GLEXT_PROTOTYPES)
-add_definitions(-DGL_GLES_PROTOTYPES)
+add_definitions(-DEGL_EGL_PROTOTYPES=1)
+add_definitions(-DEGL_EGLEXT_PROTOTYPES=1)
+add_definitions(-DGL_GLES_PROTOTYPES=1)
+add_definitions(-DGL_GLEXT_PROTOTYPES=1)
+
 include_directories(AFTER ${M3E_SOURCE_DIR}/${EXT_PATH}/angle/include)
