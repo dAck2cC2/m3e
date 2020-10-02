8e0ec0ea61964ecc7311548a2100e1c9e2c686cf
Jeremy <dAck2cC@yahoo.com>
2020-10-02 18:40:47 +0200

    build interfaces from external downloaded source code

--- cmake/hardware/interfaces/include.cmake
+++ cmake/hardware/interfaces/include.cmake
@@ -1,9 +1,9 @@
 
 # include
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/hardware/interfaces/configstore/utils/include)
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/hardware/interfaces/audio/common/all-versions/default/include)
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/hardware/interfaces/audio/common/all-versions/util/include)
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/hardware/interfaces/audio/common/2.0/default)
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/hardware/interfaces/audio/common/4.0/default)
+include_directories(AFTER ${HIDL_ROOT_PATH}/hardware/interfaces/configstore/utils/include)
+include_directories(AFTER ${HIDL_ROOT_PATH}/hardware/interfaces/audio/common/all-versions/default/include)
+include_directories(AFTER ${HIDL_ROOT_PATH}/hardware/interfaces/audio/common/all-versions/util/include)
+include_directories(AFTER ${HIDL_ROOT_PATH}/hardware/interfaces/audio/common/2.0/default)
+include_directories(AFTER ${HIDL_ROOT_PATH}/hardware/interfaces/audio/common/4.0/default)
 
 include_directories(AFTER ${TMP_PATH}/${HIDL_HEADER_PATH})
