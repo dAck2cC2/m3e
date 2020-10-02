abe57ff184598fb8d94c79b30cc75451f5c1b307
Jeremy <dAck2cC@yahoo.com>
2020-10-02 19:03:18 +0200

    build libhidl from external downloaded source code

--- cmake/system/hidl/include.cmake
+++ cmake/system/hidl/include.cmake
@@ -1,7 +1,7 @@
 
 # include
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/system/libhidl/base/include)
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/system/libhidl/libhidlmemory/include)
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/system/libhidl/transport/include)
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/system/libhidl/transport/token/1.0/utils/include)
-include_directories(AFTER ${M3E_SOURCE_DIR}/src/system/libhidl/transport/allocator/1.0/utils/include)
+include_directories(AFTER ${HIDL_ROOT_PATH}/system/libhidl/base/include)
+include_directories(AFTER ${HIDL_ROOT_PATH}/system/libhidl/libhidlmemory/include)
+include_directories(AFTER ${HIDL_ROOT_PATH}/system/libhidl/transport/include)
+include_directories(AFTER ${HIDL_ROOT_PATH}/system/libhidl/transport/token/1.0/utils/include)
+include_directories(AFTER ${HIDL_ROOT_PATH}/system/libhidl/transport/allocator/1.0/utils/include)
