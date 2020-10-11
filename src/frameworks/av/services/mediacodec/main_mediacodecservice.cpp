a0133309e023059d5215e6cf7b93126a9580b0ee
Jeremy Feng <dAck2cC@yahoo.com>
2020-10-11 11:21:03 +0200

    Update service/mediacodec to Android 9

--- src/frameworks/av/services/mediacodec/main_mediacodecservice.cpp
+++ src/frameworks/av/services/mediacodec/main_mediacodecservice.cpp
@@ -1,60 +1,15 @@
 
+// OMX implemetation
+#include <media/stagefright/omx/1.0/Omx.h>
+#include <media/stagefright/omx/1.0/OmxStore.h>
 
-#include <utils/threads.h>
-#include <binder/ProcessState.h>
-#include <binder/IPCThreadState.h>
-#include "MediaCodecService.h"
-
-#include <media/stagefright/omx/OMXNodeInstance.h>
-
-namespace android {
-    
-    class MediaCodecServiceThread : public Thread
-    {
-    public:
-        MediaCodecServiceThread() {};
-        
-        void waitForStarted(void) {
-            if (!Thread::isRunning()) {
-                mStartedMutex.lock();
-                
-                Thread::run("MediaCodecService");
-                
-                mStartedCondition.wait(mStartedMutex);
-                mStartedMutex.unlock();
-            }
-        }
-    private:
-        virtual bool threadLoop() {
-            mStartedMutex.lock();
-            
-            sp<ProcessState>  proc = ProcessState::self();
-            mService = MediaCodecService::create();
-            
-            mStartedCondition.broadcast();
-            mStartedMutex.unlock();
-            
-            IPCThreadState::self()->joinThreadPool();
-            
-            return false;
-        };
-        
-    private:
-        sp<MediaCodecService>  mService;
-        Mutex              mStartedMutex;
-        Condition          mStartedCondition;
-    };
-    
-}; // namespace android {
-
-
-//  interface for initrc
-
+// interface for initrc
 #include <cutils/compiler.h>
 #include <utils/Errors.h>
 #include <hardware/hardware.h>
 
-using namespace android;
+using namespace ::android;
+using namespace ::android::hardware::media::omx::V1_0;
 
 #define SERVICE_TAG     0xF3EC
 #define SERVICE_VER     7020
@@ -62,18 +17,11 @@ using namespace android;
 #define SERVICE_NAME    "media.codec"
 #define SERVICE_AUTHOR  "yuki.kokoto"
 
-static sp<MediaCodecServiceThread> gService;
+static sp<IOmxStore> gOmxStore;
+static sp<IOmx>      gOmx;
 
-int open_mediacodecservice(const struct hw_module_t* module, const char* id,
-                      struct hw_device_t** device)
-{
-    if (gService == NULL) {
-        gService = new MediaCodecServiceThread();
-        gService->waitForStarted();
-    }
-    
-    return OK;
-}
+static int open_mediacodecservice(const struct hw_module_t* module, const char* id, struct hw_device_t** device);
+static int close_mediacodecservice(struct hw_device_t* device);
 
 hw_module_methods_t method = {
     open_mediacodecservice
@@ -97,3 +45,51 @@ hw_module_t HMI = {
     { 0 }    // reserved
 };
 
+struct hw_device_t DEV = {
+    SERVICE_TAG, // tag
+    SERVICE_VER, // version
+    &HMI,                // module
+    { 0 },               // reserved
+    close_mediacodecservice // close
+};
+
+int open_mediacodecservice(const struct hw_module_t* module, const char* id,
+                      struct hw_device_t** device)
+{
+    // Default codec services
+    using namespace ::android::hardware::media::omx::V1_0;
+    
+    if (gOmxStore == nullptr) {
+        sp<IOmxStore> gOmxStore = new implementation::OmxStore();
+        if (gOmxStore == nullptr) {
+            ALOGE("Cannot create IOmxStore HAL service.");
+        } else if (gOmxStore->registerAsService() != OK) {
+            ALOGE("Cannot register IOmxStore HAL service.");
+        }
+    }
+    
+    if (gOmx == nullptr) {
+        sp<IOmx> gOmx = new implementation::Omx();
+        if (gOmx == nullptr) {
+            ALOGE("Cannot create IOmx HAL service.");
+        } else if (gOmx->registerAsService() != OK) {
+            ALOGE("Cannot register IOmx HAL service.");
+        } else {
+            ALOGE("IOmx HAL service created.");
+        }
+    }
+    
+    if (device != NULL) {
+        (*device) = &DEV;
+    }
+    
+    return OK;
+}
+
+int close_mediacodecservice(struct hw_device_t* device)
+{
+    gOmx = NULL;
+    gOmxStore = NULL;
+    
+    return OK;
+}
